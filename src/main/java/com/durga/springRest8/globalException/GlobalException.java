3ea7e32130e3d5e22b624dccf5726fc534aee823
Your Name <your-email@example.com>
2025-05-16 18:12:59 +0530

    spring8

--- src/main/java/com/durga/springRest8/globalException/GlobalException.java
+++ src/main/java/com/durga/springRest8/globalException/GlobalException.java
@@ -3,6 +3,8 @@ package com.durga.springRest8.globalException;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -13,10 +15,12 @@ import com.durga.springRest8.exception.StudentNotFound;
 
 @ControllerAdvice
 public class GlobalException {
+	public static final Logger LOGGER=LoggerFactory.getLogger(GlobalException.class);
 	@ExceptionHandler(exception= {StudentNotFound.class,StudentAlreadyFound.class})
 	public ResponseEntity<Object> exception(Exception e){
 		
 		Map<String,String> error=new HashMap<String,String>();
+		LOGGER.error("Student Details already existed: "+e.getMessage());
 		error.put("message",e.getMessage());
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
 	}
